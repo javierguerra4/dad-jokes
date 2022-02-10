7595d4253331429598f1d397091996bbc45944b9
Javier Guerra <javier.j.guerra4@gmail.com>
2022-02-09 23:58:15 -0600

    received a response in console log

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 'use strict'
 let response = fetch('https://icanhazdadjoke.com/')
-    .then(response => response.json())
-    .then(data => console.log(data))
+    .then(response => {
+        console.log(response)
+    })
     .catch(error => {
         console.log('You have an error')
     })
